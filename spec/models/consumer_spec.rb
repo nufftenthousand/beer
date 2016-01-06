98e0d502437e1cc3c850d8aa55e93d854378d9dc
Nate Duffy <nate.duffy@gmail.com>
2016-01-06 18:27:47 -0500

    weird, consumer_spec wasn't merged properly... fixed

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe Consumer, type: :model do
 
   it "has a valid factory" do
-    expect(build(:consumer)).to be_valid
+    expect(build_stubbed(:consumer)).to be_valid
+  end
+
+  let(:consumer_stub) { build_stubbed(:consumer) }
+
+  it "is an instance of User" do
+    expect(consumer_stub).to be_a User
   end
 
 end
